2d6f4997baf6944f5a8b510ae1694fe4e1848271
Kirill Kumchenko <kirillkumchenko@gmail.com>
2024-04-28 13:05:18 +0300

    fix: enhance order router

--- src/routes/order.ts
+++ src/routes/order.ts
@@ -5,17 +5,17 @@ const router = Router()
 const authRouter = Router()
 
 // Middlewares
-authRouter.use('/order/:id', OrderController.check)
+authRouter.use('/:id', OrderController.check)
 
 // Non-auth Handlers
 router.post('/', OrderController.create)
-router.get('/order/:id', OrderController.check, OrderController.getWithoutAuth)
+router.get('/:id', OrderController.check, OrderController.getWithoutAuth)
 
 // Auth Handlers
 authRouter.get('/stats/', OrderController.getStats)
-authRouter.get('/order/:id', OrderController.get)
-authRouter.patch('/order/:id', OrderController.update)
-authRouter.delete('/order/:id', OrderController.delete)
+authRouter.get('/:id', OrderController.get)
+authRouter.patch('/:id', OrderController.update)
+authRouter.delete('/:id', OrderController.delete)
 authRouter.get('/', OrderController.getMany)
 
 export { router as orderRouter, authRouter as orderAuthRouter }
